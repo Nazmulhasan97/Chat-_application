097dae57d869c9a857a036c2026431a29be26899
Nazmul Hasan <md.nazmul.usk97@gmail.com>
2021-11-07 23:12:39 +0600

    new

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 
 // set static folder :
+app.use(express.static(path.join(__dirname , 'public')));
 
-app.use(express.static(path.join(__dirname , ' public')));
+// run when cilent connected:
+io.on('connection' , socket => {
+    console.log('New WS connection...');
+});
 
 const PORT = process.env.PORT || 3000;
 
